2474bd2f91be503e6d9b5108cb2f71921b464273
Vamsi <s.vamsirao@gmail.com>
2017-02-21 16:45:52 +0530

    Create dialog box for the activity

--- app/src/main/java/com/example/vamsirao/rating/MainActivity.java
+++ app/src/main/java/com/example/vamsirao/rating/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.vamsirao.rating;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
@@ -10,15 +12,35 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        final Dialog dialogBox=new Dialog(MainActivity.this);
+        dialogBox.setContentView(R.layout.activity_main);
+        dialogBox.setCancelable(true);
+        dialogBox.show();
+
+        final RatingBar ratingBar= (RatingBar)dialogBox.findViewById(R.id.rating_bar);
+
+        Button button= (Button)dialogBox.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String rating=String.valueOf(ratingBar.getRating());
+                Toast.makeText(MainActivity.this,"You've rated "+rating+" stars",Toast.LENGTH_SHORT).show();
+                dialogBox.dismiss();
+            }
+        });
+
+
+
+
     }
 
-    public void  onClick(View view){
 
-        RatingBar ratingBar= (RatingBar) findViewById(R.id.rating_bar);
 
-        String rating=String.valueOf(ratingBar.getRating());
-        Toast.makeText(this,"You've rated "+rating+" stars",Toast.LENGTH_SHORT).show();
-    }
+
 }
